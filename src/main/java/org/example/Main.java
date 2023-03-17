d74837dfefc011f52bf1341253547ca66a6973dd
Jose Pena <>
2023-03-17 13:13:08 -0300

    First Homework from java course

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("1. Games");
+        System.out.println("2. Home Video Collection");
+        System.out.println("3. Flight Plans");
+        System.out.println("4. Work Documents");
+        System.out.println("5. Exit");
+
+    }
+}
